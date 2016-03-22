3f1c8ebf7fade134b2fe198e88a1a10936dc52d5
Cesar Gonzalez <chicharo_man_x@hotmail.com>
2016-03-22 18:09:00 -0500

    Se agrega bootstrap y entity framework

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Linq;
+using TheWorld.Models;
+using TheWorld.Services;
+using TheWorld.ViewModel;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private WorldContext _context;
+
+        public AppController(IMailService service, WorldContext context)
+        {
+            _mailService = service;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var trips = _context.Trips.OrderBy(t => t.Name).ToList();
+            return View(trips);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if(string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration problem");
+                }
+
+                if(_mailService.SendMail(email,
+                    email,
+                    $"Contact Page from {model.Name} ({model.Email})",
+                    model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail Sent. Thanks!";
+
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Options()
+        {
+            return View();
+        }
+    }
+}
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -5,23 +5,58 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TheWorld.Services;
+using Microsoft.Extensions.PlatformAbstractions;
+using TheWorld.Models;
 
 namespace TheWorld
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<WorldContext>();
+
+
+#if DEBUG
+            services.AddScoped<IMailService, DebugMailService>();
+#else
+            services.AddScoped<IMailService, RealMailService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+            //app.UseDefaultFiles();
+            app.UseStaticFiles();//Se queda por los css, js, etc.
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                        name: "Default",
+                        template: "{controller}/{action}/{id?}",
+                        defaults: new { controller = "App", action = "Index" }
+                        );
+            });
 
             //app.Run(async (context) =>
             //{
